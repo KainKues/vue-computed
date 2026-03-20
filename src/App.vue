cb770900a75cdc935d7f0fdd70eb35b9f7419944
KainKues <kainkues@gmail.com>
2026-03-20 14:43:41 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>  
+  import F from "./components/F.vue"
+</script>
+ 
+<template>
+  <F/>
+</template>
+
+<style>
+  body{
+    background-color: gray;
+  }
+</style>
---
+++ src/components/F.vue
@@ -0,0 +1,26 @@
+<script>
+import {ref, computed} from 'vue'
+let name = ref('')
+let alertMes = computed(()=> {
+    return `Привет, ${name.value}!`
+})
+
+</script>
+
+<template>
+    <input type="text"
+            class="input"
+            v-model = 'name'>
+    <div class="message"
+            v-if="name">{{ alertMes }}</div>
+            
+</template>
+
+<style>
+    .message{
+        background-color: white;
+        color: black;
+        width: 200px;
+        height: 20px;
+    }
+</style>
