8c6651d588717024b766dc74d9ba979cc371624a
KainKues <kainkues@gmail.com>
2026-03-20 15:15:47 +0300

    Fix: Add template/script to S.vue and update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <script setup>  
   import F from "./components/F.vue"
+  import S from "./components/S.vue"
 </script>
  
 <template>
-  <F/>
+  <div class="field">
+    <div class="block">
+      <F></F>
+    </div>
+    <div class="block">
+      <S></S>
+    </div>
+  </div>
 </template>
 
 <style>
   body{
     background-color: gray;
   }
+  .field{
+    display: flex;
+    flex-direction: column;
+    gap: 40px
+  }
+
 </style>
---
+++ src/components/S.vue
@@ -0,0 +1,26 @@
+<script>
+import {ref, computed} from 'vue'
+let text = ref('')
+let sum = computed(()=> {
+    return text.value.length;
+})
+
+</script>
+
+<template>
+    <input type="text"
+            class="input"
+            v-model = 'text'>
+    <div class="message"
+            v-if="sum">Количество символов: {{ sum }}</div>
+            
+</template>
+
+<style>
+    .message{
+        background-color: white;
+        color: black;
+        width: 200px;
+        height: 20px;
+    }
+</style>
